f4c07cd54723d2242741b71c3f1ad22b5d598b44
Avin Dev <avindevelop@gmail.com>
2023-10-29 09:30:17 +0330

    works for mahtab

--- lib/ChannelPage.dart
+++ lib/ChannelPage.dart
@@ -14,26 +14,22 @@ class ChannelPage extends StatefulWidget {
 }
 
 class _ChannelPageState extends State<ChannelPage> {
-  List<Map<String, dynamic>> channelMessages = []; // List to store channel data
-
+  List<Map<String, dynamic>> channelMessages = [];
   late Future fetchDataFuture;
 
   @override
-  initState() {
+  void initState() {
     super.initState();
     fetchDataFuture = getInfo();
   }
 
   Future<void> getInfo() async {
     final response = await ApiUtil.getRequest('channel/${widget.id}/messages');
-    print('API Response: $response'); // Print the response
+    print('API Response: $response');
     if (response['messages'] is List) {
-      // Update channelData with the response data
-      print("is a list");
       setState(() {
         channelMessages = List<Map<String, dynamic>>.from(response['messages']);
       });
-      print(channelMessages);
     } else {
       throw Exception('Invalid response format');
     }
@@ -41,19 +37,24 @@ class _ChannelPageState extends State<ChannelPage> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: fetchDataFuture,
-      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-        return Scaffold(
-          resizeToAvoidBottomInset: false,
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            title: Text(
-              widget.title,
-              textAlign: TextAlign.right,
-            ),
-          ),
-          body: SingleChildScrollView(
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(
+          widget.title,
+          textAlign: TextAlign.right,
+        ),
+      ),
+      body: FutureBuilder(
+        future: fetchDataFuture,
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator(); // Show a loading indicator while fetching data.
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return SingleChildScrollView(
               child: SafeArea(
                 child: Container(
                   width: Helper.getScreenWidth(context),
@@ -61,52 +62,54 @@ class _ChannelPageState extends State<ChannelPage> {
                     padding: EdgeInsets.all(20.0),
                     child: Column(
                       children: [
-
                         ListView.builder(
                           shrinkWrap: true,
                           physics: NeverScrollableScrollPhysics(),
                           itemCount: channelMessages.length,
                           itemBuilder: (context, index) {
                             final message = channelMessages[index];
-                            if(message['type'] != "text")
-                              print(message);
-                            if(message['type'] == "text")
-                            return Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white60,
-                                    borderRadius: BorderRadius.all(Radius.circular(5)),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.black26.withOpacity(0.1),
-                                        spreadRadius: 0,
-                                        blurRadius: 5,
-                                        offset: Offset(0, 5),
-                                      ),
-                                    ],
+                            if (message['type'] == "text") {
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.white60,
+                                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black26.withOpacity(0.1),
+                                          spreadRadius: 0,
+                                          blurRadius: 5,
+                                          offset: Offset(0, 5),
+                                        ),
+                                      ],
+                                    ),
+                                    padding: EdgeInsets.all(10),
+                                    width: Helper.getScreenWidth(context) * 0.5,
+                                    child: Text(message['text']),
                                   ),
-                                  padding: EdgeInsets.all(10),
-                                  width: Helper.getScreenWidth(context)*0.5,
-                                  child: Text(message['text']),
-                                ),
-                                SizedBox(
-                                  height: 20,
-                                )
-                              ],
-                            );
+                                  SizedBox(
+                                    height: 20,
+                                  )
+                                ],
+                              );
+                            } else {
+                              print(message);
+                              return Container(); // Handle other message types or skip them.
+                            }
                           },
-                        )
+                        ),
                       ],
                     ),
                   ),
                 ),
-              )),
-          // This trailing comma makes auto-formatting nicer for build methods.
-        );
-      },
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
